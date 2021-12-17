87bdf045cae30b73068d73df43aa58fde8d3091d
sindhusaranyar@gmail.com <sindhusaranyar@gmail.com>
2021-12-17 22:46:02 +0530

    initial commit

---
+++ pattern.js
@@ -0,0 +1,16 @@
+var x=9;
+var y="";
+for(var i=0;i<=x;i++){
+    for(var j=0;j<i;j++)
+    y=y+"*";
+    
+    y=y+"\n";
+}
+for(var k=0;k<=x-1;k++){
+    for (l=0;l<=x-k-2;l++){
+        y=y+"*";
+    }
+y=y+"\n";
+}
+        console.log(y);
+
